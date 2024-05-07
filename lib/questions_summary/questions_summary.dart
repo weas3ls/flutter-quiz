12e64c361a0b680a4175c72cde1dd83b2002e605
weas3ls <shabaneh.ahmad@gmail.com>
2024-05-07 03:35:04 -0700

    final project

---
+++ lib/questions_summary/questions_summary.dart
@@ -0,0 +1,24 @@
+import 'package:adv_basics/questions_summary/summary_item.dart';
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData
+              .map(
+                (data) => SummaryItem(data),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
